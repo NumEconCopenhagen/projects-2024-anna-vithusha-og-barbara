9b4ea7cebf340fcee101ec0e4297655827d91712
YOUR GITHUB USERNAMEBarbaraBille <YOUR E-MAILbtagmose@gmail.com>
2024-03-17 17:40:33 +0100

    færdig for nu

--- inauguralproject/ExchangeEconomy.py
+++ inauguralproject/ExchangeEconomy.py
@@ -15,6 +15,12 @@ class ExchangeEconomyClass:
         par.w1A = 0.8
         par.w2A = 0.3
 
+        # tolerence
+
+        par.eps = 1e-8
+        par.maxiter = 1000
+        par.kappa = 0.1
+
     def utility_A(self,x1A,x2A):
         par = self.par
 
@@ -76,6 +82,62 @@ class ExchangeEconomyClass:
         return x1a_vec, x2a_vec
 
                     
+    def find_equilibrium(self,p1_guess,p2, w1A, w2A):
+            
+            par = self.par
 
+            par.w1A = w1A
+            par.w2A = w2A
+            
+            t = 0
+            p1 = p1_guess
+            
+            # using a while loop as we don't know number of iterations a priori
+            while True:
+
+                # a. step 1: excess demand
+                Z1 = self.check_market_clearing(p1)[0]
+                
+                # b: step 2: stop?
+                if  np.abs(Z1) < par.eps or t >= par.maxiter:
+                    print(f'{t:3d}: p1 = {p1:12.8f} -> excess demand -> {Z1:14.8f}')
+                    break    
+                
+                # d. step 3: update p1
+                p1 = p1 + par.kappa*Z1/2
+                
+                # e. step 4: update counter and return to step 1
+                t += 1    
+
+
+            # Check if solution is found 
+            if np.abs(Z1) < par.eps:
+                # Store equilibrium prices
+                self.p1_star = p1 
+
+                # Store equilibrium excess demand 
+                self.Z1 = Z1
+                self.Z2 = self.check_market_clearing(self.p1_star)[1]
+
+                # Make sure that Walras' law is satisfied
+                if not np.abs(self.Z2)<par.eps:
+                    print('The market for good 2 was not cleared')
+                    print(f'Z2 = {self.Z2}')
+
+            else:
+                print('Solution was not found')
+
+
+    def print_solution(self):
+
+        text = 'Solution to market equilibrium:\n'
+        text += f'p1 = {self.p1_star:5.3f}\n\n\n'
+
+        text += 'The allocations are:\n'
+        text += f'(x1A, x2A) = ({self.demand_A(self.p1_star)})\n'
+        text += f'(x1B, x2B) = ({self.demand_B(self.p1_star)})\n'
+        print(text)
+
+        return self.demand_A(self.p1_star)
 
 
